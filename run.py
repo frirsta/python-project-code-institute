6a430f2a702ec987ce8291c47dbe1b454e13ec35
frirsta <frista-8@hotmail.com>
2022-10-16 04:39:40 +0000

    Add validate_data function to convert user input to integer or raise ValueError if input is not a number

--- run.py
+++ run.py
@@ -16,16 +16,38 @@ SHEET = GSPREAD_CLIENT.open("python-project-code-institute")
 
 age = SHEET.worksheet("customer_input")
 
+
+def validate_data(value):
+    """
+    Converts user input string to integer.
+    Raises ValueError if string can not be converted.
+    """
+    try:
+        [int(value)]
+    except ValueError as e:
+        print(f"Invalid data: {e}, try again")
+        return False
+    
+    return True
+
+
 def get_age_data():
     """
     Get age
     """
-    print("Enter age here")
-    print("Data should be number")
-    print("Example: 55")
+    while True:
+        print("Enter age here")
+        print("Data should be number")
+        print("Example: 55")
 
-    data_str = input("Enter age here: \n")
+        data_str = input("Enter age here: \n")
+
+        if validate_data(data_str):
+            print("Valid")
+            break
+
+    return data_str
+
     
 
 get_age_data()
-

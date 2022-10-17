bc290ca638978ad40e295273a51253130a50cc6a
frirsta <frista-8@hotmail.com>
2022-10-17 07:44:06 +0000

    Add update_age_group function that adds the number 1 to the age_group the user input belongs to

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+"""
+Age
+"""
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -14,7 +17,8 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("python-project-code-institute")
 
 
-age = SHEET.worksheet("customer_input")
+customer_input = SHEET.worksheet("customer_input")
+age_group = SHEET.worksheet("age_group")
 
 
 def validate_data(value):
@@ -24,17 +28,18 @@ def validate_data(value):
     """
     try:
         [int(value)]
-    except ValueError as e:
-        print(f"Invalid data: {e}, try again")
+    except ValueError as event:
+        print(f"Invalid data: {event}, try again")
         return False
-    
+
     return True
 
 
 def get_age_data():
     """
     Get age from user.
-    Executes a while loop to gather a valid string of data through the terminal.
+    Executes a while loop to gather a
+    valid string of data through the terminal.
     Must be numbers in the string.
     """
     while True:
@@ -50,7 +55,6 @@ def get_age_data():
 
     return data_str
 
-    
 
 age_data = [get_age_data()]
 
@@ -59,9 +63,21 @@ def update_woorksheet(data):
     """
     Update customer_input sheet with user input.
     """
-    customer_input = SHEET.worksheet('customer_input')
     customer_input.append_row(data)
     print("Updated!")
 
 
 update_woorksheet(age_data)
+
+
+def update_age_group(data):
+    """
+    abc
+    """
+    num = [1]
+    if int(data[0]) <= 18:
+        return age_group.append_row(num)
+    print("done")
+
+
+update_age_group(age_data)
